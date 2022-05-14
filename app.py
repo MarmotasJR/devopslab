6c1f91b051dac9774f0acc9ce475826006a503f6
MarmotasJR <102331136+MarmotasJR@users.noreply.github.com>
2022-05-14 12:18:36 -0300

    Mensagem

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Essa aula e muito TOP! Obrigado prof! - Marmotas"
+    return "Essa aula"
 
 if __name__ == '__main__':
     app.run()
